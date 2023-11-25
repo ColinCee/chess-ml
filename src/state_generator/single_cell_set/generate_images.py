4d84b451448529f8be4f5dde930a73cc74b68585
Colin Cheung <11246116+ColinCee@users.noreply.github.com>
2023-11-25 19:46:10 +0000

    CREATE empty squares

--- src/state_generator/single_cell_set/generate_images.py
+++ src/state_generator/single_cell_set/generate_images.py
@@ -14,12 +14,15 @@ pieces_path = Path(__file__).parent / "../piece-sets/lichess-standard/"
 output_path = Path(__file__).parent / "training_images"
 
 
-def create_cell_svg(background_color: str, piece: str):
+def create_cell_svg(background_color: str, piece: str | None = None):
     d = draw.Drawing(64, 64, origin=(0, 0))
     d.append(draw.Rectangle(0, 0, 64, 64, fill=background_color))
 
     piece_size = 64  # Adjust as needed
     piece_padding = (64 - piece_size) / 2
+    if piece is None:
+        return d.save_png(f"{output_path}/empty_{background_color}.png")
+
     image = draw.Image(
         piece_padding,
         piece_padding,
@@ -36,3 +39,6 @@ def create_cell_svg(background_color: str, piece: str):
 for piece in pieces:
     for color in [light_square_color, dark_square_color]:
         svg = create_cell_svg(color, piece)
+
+create_cell_svg(light_square_color)
+create_cell_svg(dark_square_color)
