d1957494ca62eac61632003362bc0c5c6db60dec
Colin Cheung <11246116+ColinCee@users.noreply.github.com>
2023-11-26 12:08:57 +0000

    Add pyvips library for image processing

--- src/neural_net/main.py
+++ src/neural_net/main.py
@@ -7,56 +7,7 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 
-
-def train_loop(dataloader, model, loss_fn, optimizer):
-    size = len(dataloader.dataset)
-    # Set the model to training mode - important for batch normalization and dropout layers
-    # Unnecessary in this situation but added for best practices
-    model.train()
-    for batch, (X, y) in enumerate(dataloader):
-        # Move the data to the GPU
-        X = X.to(device)
-        y = y.to(device)
-
-        # Compute prediction and loss
-        pred = model(X)
-        loss = loss_fn(pred, y)
-
-        # Backpropagation
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-
-        if (batch / size) % 0.1 == 0:
-            loss, current = loss.item(), (batch + 1) * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-
-
-def test_loop(dataloader, model, loss_fn):
-    # Set the model to evaluation mode - important for batch normalization and dropout layers
-    # Unnecessary in this situation but added for best practices
-    model.eval()
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    test_loss, correct = 0, 0
-
-    # Evaluating the model with torch.no_grad() ensures that no gradients are computed during test mode
-    # also serves to reduce unnecessary gradient computations and memory usage for tensors with requires_grad=True
-    with torch.no_grad():
-        for X, y in dataloader:
-            # Move the data to the GPU
-            X = X.to(device)
-            y = y.to(device)
-
-            pred = model(X)
-            test_loss += loss_fn(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-
-    test_loss /= num_batches
-    correct /= size
-    print(
-        f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n"
-    )
+from src.neural_net.training import test_loop, train_loop
 
 
 device = torch.device("cuda:0")
@@ -64,7 +15,6 @@ model = ChessboardFENNet()
 model.to(device)
 
 # Assuming that we are on a CUDA machine, this should print a CUDA device:
-
 print(device)
 
 # Instantiate the dataset
@@ -89,8 +39,8 @@ optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 epochs = 10
 for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
-    train_loop(train_dataloader, model, loss_fn, optimizer)
-    test_loop(test_dataloader, model, loss_fn)
+    train_loop(train_dataloader, model, loss_fn, optimizer, device)
+    test_loop(test_dataloader, model, loss_fn, device)
 
 print("Done!")
 
--- src/state_generator/single_cell_set/generate_images.py
+++ src/state_generator/single_cell_set/generate_images.py
@@ -1,8 +1,13 @@
 from io import BytesIO
+from math import pi
+import os
 from pathlib import Path
+import time
 from PIL import Image
 import cairosvg
 import drawsvg as draw
+from matplotlib.pylab import f
+import pyvips
 
 
 # light_square_color = "#f0d9b5"
@@ -11,18 +16,36 @@ import drawsvg as draw
 light_square_color = "#ffffdd"
 dark_square_color = "#86a666"
 
+piece_to_lichess_filename = {
+    "b": "bB",
+    "k": "bK",
+    "n": "bN",
+    "p": "bP",
+    "q": "bQ",
+    "r": "bR",
+    "B": "wB",
+    "K": "wK",
+    "N": "wN",
+    "P": "wP",
+    "Q": "wQ",
+    "R": "wR",
+}
 pieces = ["b", "k", "n", "p", "q", "r", "B", "K", "N", "P", "Q", "R"]
 
-pieces_path = Path(__file__).parent / "../piece-sets/lichess-standard/"
-output_path = Path(__file__).parent / "testing_images"
+pieces_path = Path(__file__).parent / f"../../../../lila/public/piece/"
 
 
-def create_cell_svg(background_color: str, piece: str | None = None):
+
+def create_cell_svg(background_color: str, piece_set_name, piece: str | None = None):
     d = draw.Drawing(64, 64, origin=(0, 0))
     d.append(draw.Rectangle(0, 0, 64, 64, fill=background_color))
 
     piece_size = 64  # Adjust as needed
     piece_padding = (64 - piece_size) / 2
+    output_path = Path(__file__).parent / "training_images" / piece_set_name
+    # create output folder if it doesn't exist
+    output_path.mkdir(parents=True, exist_ok=True)
+    
     if piece is None:
         return d.save_png(f"{output_path}/empty_{background_color}.png")
 
@@ -31,17 +54,36 @@ def create_cell_svg(background_color: str, piece: str | None = None):
         piece_padding,
         piece_size,
         piece_size,
-        f"{pieces_path}/{piece}.svg",
+        f"{pieces_path}/{piece_set_name}/{piece_to_lichess_filename[piece]}.svg",
         embed=True,
     )
-    d.append(draw.Use(image, 0, 0, transform="scale(1.42222)"))
+    d.append(draw.Use(image, 0, 0))
+    
+    # save using cairosvg
+    svg = d.as_svg()
+    if not svg:
+        raise Exception("SVG is empty")
 
-    return d.save_png(f"{output_path}/{piece}_{background_color}.png")
+    # use libvips to convert svg to png
+    image = pyvips.Image.new_from_buffer(svg.encode("utf8"), "", access="sequential")
+    image.write_to_file(f"{output_path}/{piece}_{background_color}.png") # type: ignore
+        
+
+# get all subfolders folders in pieces_path
 
 
-for piece in pieces:
-    for color in [light_square_color, dark_square_color]:
-        svg = create_cell_svg(color, piece)
+# Get a list of all files and directories in the path
+all_files_and_dirs = os.listdir(pieces_path)
+# Filter out any files, leaving only directories
+dirs = [dir for dir in all_files_and_dirs if os.path.isdir(os.path.join(pieces_path, dir))]
+# filter out mono
+dirs = [dir for dir in dirs if dir != "mono"]
 
-create_cell_svg(light_square_color)
-create_cell_svg(dark_square_color)
+print(dirs)  # Print the list of directories
+for piece_set_name in dirs:
+    for piece in pieces:
+        for color in [light_square_color, dark_square_color]:
+            svg = create_cell_svg(color, piece_set_name, piece)
+
+    create_cell_svg(light_square_color, piece_set_name)
+    create_cell_svg(dark_square_color, piece_set_name)
